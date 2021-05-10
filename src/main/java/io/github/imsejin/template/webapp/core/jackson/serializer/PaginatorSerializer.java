186401fc9ee9ce802fe771cd34733a2a2a852fb2
ImSejin <hackeres123@gmail.com>
2021-05-10 17:24:05 +0900

    ✨ Add: serializier for paginator

---
+++ src/main/java/io/github/imsejin/template/webapp/core/jackson/serializer/PaginatorSerializer.java
@@ -0,0 +1,32 @@
+package io.github.imsejin.template.webapp.core.jackson.serializer;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+import io.github.imsejin.template.webapp.core.database.mybatis.model.Paginator;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
+
+import java.io.IOException;
+
+public class PaginatorSerializer extends StdSerializer<Paginator<?>> {
+
+    /**
+     * Prevent {@link HttpMediaTypeNotAcceptableException} from occurring.
+     */
+    public PaginatorSerializer() {
+        this(null);
+    }
+
+    public PaginatorSerializer(Class<Paginator<?>> type) {
+        super(type);
+    }
+
+    @Override
+    public void serialize(Paginator<?> paginator, JsonGenerator gen, SerializerProvider provider) throws IOException {
+        gen.writeStartObject();
+        gen.writeObjectField("page", paginator.getPage());
+        gen.writeObjectField("items", paginator.getItems());
+        gen.writeEndObject();
+    }
+
+}
